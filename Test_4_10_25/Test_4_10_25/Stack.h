69c1d556d395bbca8642aa833f6e68a50965df79
Jason-of-carriben <1802322187@qq.com>
2025-04-10 22:54:31 +0800

    类和对象（2）链接问题

---
+++ Test_4_10_25/Test_4_10_25/Stack.h
@@ -0,0 +1,21 @@
+#pragma once
+
+struct Stack
+{
+public:
+	int top;
+	int capacity;
+	int* s;
+};
+
+void InitStack(Stack& st)
+{
+	st.s = nullptr;
+	st.capacity = 4;
+	st.top = -1;
+}
+
+void PushStack(Stack& st, int val)
+{
+	st.s[++st.top] = val;
+}
---
+++ Test_4_10_25/Test_4_10_25/test.cpp
@@ -0,0 +1,9 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include "Stack.h"
+
+int main()
+{
+	Stack st;
+	InitStack(st);
+	return 0;
+}
