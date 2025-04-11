4785985b24d362d2492ab9ba84070f559327f585
Jason-of-carriben <dongsijie8@gmail.com>
2025-04-11 23:11:13 +0800

    类和对象（2）

--- Test_4_11_25/Test_4_11_25/test.cpp
+++ Test_4_11_25/Test_4_11_25/test.cpp
@@ -4,35 +4,121 @@
 #include <string>
 using namespace std;
 
-#define FUNC_PTR_TYPE(RETURN_TYPE, FUNC_PTR_NAME, ...) \
-  typedef RETURN_TYPE (*FUNC_PTR_NAME)(__VA_ARGS__)
-
-#define TOSTRING(a) printf(#a " = %d\n", a)
-
-FUNC_PTR_TYPE(int, INTFUNCPTR, int, int);
-FUNC_PTR_TYPE(void, VOIDFUNCPTR);
-int add(int a, int b)
+class Person
 {
-	return a + b;
-}
+public:
+	void func();
 
-void print()
-{
-	cout << "void print" << endl;
-}
-
+};
 int main()
 {
-	INTFUNCPTR func1 = add;
-	VOIDFUNCPTR func2 = print;
-	int a = 29;
-	int b = 29;
-	TOSTRING(a + b);
-	/*int ret = func1(29, 29);
-	cout << ret << endl;*/
-	func2();
+	cout << "sizeof(Person) = " << sizeof(Person) << endl;
 	return 0;
 }
+
+//#define FUNC_PTR_TYPE(RETURN_TYPE, FUNC_PTR_NAME, ...) \
+//typedef RETURN_TYPE (*FUNC_PTR_NAME)(__VA_ARGS__);
+//
+//#define ARRAY_PTR_TYPE(ARRAY_TYPE, ARRAY_PTR_NAME, ARRAY_NUM) \
+//typedef ARRAY_TYPE (*ARRAY_PTR_NAME)[ARRAY_NUM];
+//
+//
+//int multiply(int a, int b)
+//{
+//    return a * b;
+//}
+//
+//typedef int(*aptr)[4];
+//int main()
+//{
+//	auto fpt = multiply;
+//	cout << "typeid(fpt).name()" << typeid(fpt).name() << endl;
+//	int arr1[4] = {1,2,3,4};
+//	aptr ptr1 = &arr1;
+//	auto auptr = &arr1;
+//	cout << "typeid(auptr).name()" << typeid(auptr).name() << endl;
+//	cout <<"ptr1 = " << ptr1 << endl;
+//	cout << "*ptr1 = " << *(*ptr1) << endl;
+//	cout << "ptr1 + 1 = " << ptr1 + 1 << endl;
+//	cout << "typeid(ptr1).name() : " << typeid(ptr1).name() << endl;
+//	cout << "typeid(*ptr1).name() : " << typeid(*ptr1).name() << endl;
+//	cout << "typeid(*(*ptr1)).name() : " << typeid(*(*ptr1)).name() << endl;
+//
+//	int arr2[5] = { 2,3,6,9,10 };
+//	ARRAY_PTR_TYPE(int, aptr2, 5);
+//	aptr2 ptr2 = &arr2;
+//	cout << typeid(ptr2).name() << endl;
+//	return 0;
+//}
+
+//int multiply(int a, int b)
+//{
+//    return a * b;
+//}
+//
+//void print()
+//{
+//    cout << "void print()" << endl;
+//}
+//
+//FUNC_PTR_TYPE(int, INT_FUNC_PTR, int, int);
+//FUNC_PTR_TYPE(void, VOID_FUNC_PTR);
+//
+//int main()
+//{
+//    INT_FUNC_PTR func1 = multiply;
+//    VOID_FUNC_PTR func2 = print;
+//    func1(2, 4);
+//    func2();
+//    cout << typeid(func1).name() << endl;
+//    return 0;
+//}
+
+//#define FUNC_PTR_TYPE(RETURN_TYPE, FUNC_PTR_NAME, ...) \
+//  typedef RETURN_TYPE (*FUNC_PTR_NAME)(__VA_ARGS__)
+//
+//#define TOSTRING(a) printf(#a " = %d\n", a)
+//
+//FUNC_PTR_TYPE(int, INTFUNCPTR, int, int);
+//FUNC_PTR_TYPE(void, VOIDFUNCPTR);
+//
+//typedef int(*ffptr)(int, int);
+//
+//int add(int a, int b)
+//{
+//    return a + b;
+//}
+//
+//int main()
+//{
+//    //正常写的函数指针
+//    int (*fptr)(int, int) = add;
+//    // typedef 的格式
+//    ffptr func1 = add;
+//}
+
+//int add(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//void print()
+//{
+//	cout << "void print" << endl;
+//}
+//
+//int main()
+//{
+//	INTFUNCPTR func1 = add;
+//	VOIDFUNCPTR func2 = print;
+//	int a = 29;
+//	int b = 29;
+//	TOSTRING(a + b);
+//	/*int ret = func1(29, 29);
+//	cout << ret << endl;*/
+//	func2();
+//	return 0;
+//}
 
 
 //typedef void(*ffptr)(int, string);
