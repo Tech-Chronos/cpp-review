02b2e8b0420d245b6d814680beda47aaa2631568
Jason-of-carriben <dongsijie8@gmail.com>
2025-04-18 00:35:35 +0800

    类和对象终

--- Test_4_17/Test_4_17/test.cpp
+++ Test_4_17/Test_4_17/test.cpp
@@ -5,20 +5,28 @@ using namespace std;
 class A
 {
 public:
-	friend ostream operator<<(ostream& out, A& a);
+	A() = default;
+	A(int a, double b)
+		:_a(a),
+		_b(b)
+	{}
+
+	friend ostream& operator<<(ostream& out, A& a);
 private:
 	int _a = 5;
 	double _b = 20;
 };
 
-ostream operator<<(ostream& out, A& a)
+ostream& operator<<(ostream& out, A& a)
 {
-	out << a._a << a._b << endl;
+	out << a._a << " " << a._b;
 	return out;
 }
 
 int main()
 {
-
+	A a = { 2, 3.0 };
+	A b;
+	cout << a << b << endl;
 	return 0;
 }
