fe0275d3def222ec7cfca17ff61c32a7f3344516
Jason-of-carriben <dongsijie8@gmail.com>
2025-04-21 23:50:45 +0800

    内存管理

---
+++ Test_4_21_25/Test_4_21_25/test.cpp
@@ -0,0 +1,38 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+class MyClass
+{
+public:
+	MyClass(int a = 0, int b = 0, int c = 0)
+	{
+		_a = a;
+		_b = b;
+		_c = c;
+		cout << "MyClass() -> _a = " << _a << endl;
+		cout << "MyClass() -> _b = " << _b << endl;
+		cout << "MyClass() -> _c = " << _c << endl;
+	}
+	/*~MyClass()
+	{
+		cout << "~MyClass()" << endl;
+	}*/
+private:
+	int _a;
+	int _b;
+	int _c;
+};
+int main(int argc, char* argv[])
+{
+	/*cout << argc << endl;
+	cout << *argv[0] << endl;*/
+	MyClass* obj = new MyClass[3]{ {2,3,4} };
+	int* ptr = new int(9);
+	cout << *ptr << endl;
+
+	/*MyClass* obj = (MyClass*)operator new(sizeof MyClass * 4);
+	new(obj)MyClass({1,2,3});
+	new(obj + 1)MyClass({4,3,5});*/
+	return 0;
+}
