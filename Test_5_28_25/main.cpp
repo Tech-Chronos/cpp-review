e999015bc62294925b3f33acfb6edc3e63891e9a
有趣的中国人 <dongsijie8@gmail.com>
2025-05-29 00:22:06 +0800

    杨辉三角OJ

--- Test_5_28_25/main.cpp
+++ Test_5_28_25/main.cpp
@@ -1,22 +1,52 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 
 int main()
 {
-    std::vector<int> v(10, 9);
-    for (auto it : v)
-    {
-        std::cout << it << " ";
-    }
+//    vector<vector<int>> vv;
+//    vv.reserve(5);
+//    vv.at(1);
+    vector<int> v(3);
     return 0;
 }
+//class Solution {
+//public:
+//    vector<vector<int>> generate(int numRows)
+//    {
+//        vector<vector<int>> vv(numRows);
+//        for (size_t i = 0; i < numRows; ++i)
+//        {
+//            vv[i].resize(i+1);
+//            vv[i][0] = vv[i][i] = 1;
+//        }
+//        for (size_t i = 0; i < numRows; ++i)
+//        {
+//            for (size_t j = 0; j < i; ++j)
+//            {
+//                vv[i][j] = vv[i - 1][j - 1] + vv[i - 1][j];
+//            }
+//        }
+//        return vv;
+//    }
+//};
+
+//int main()
+//{
+//    std::vector<int> v(10, 9);
+//    for (auto it : v)
+//    {
+//        std::cout << it << " ";
+//    }
+//    return 0;
+//}
 
 //int main()
 //{
 ////    std::string str("hello world");
 ////    std::cout << sizeof(str) << std::endl;
-////std::cout << sizeof(char*) << std::endl;
-//    std::string const str("hello Linux");
-//    return 0;
+////    std::cout << sizeof(char*) << std::endl;
+//      std::string const str("hello Linux");
+//      return 0;
 //}
