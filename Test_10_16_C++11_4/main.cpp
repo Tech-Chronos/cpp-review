c71eb97cfc8897204d60be4d815e0b7a3f12cbc9
有趣的中国人 <dongsijie8@gmail.com>
2025-10-16 23:23:10 +0800

    线程

--- Test_10_16_C++11_4/main.cpp
+++ Test_10_16_C++11_4/main.cpp
@@ -39,15 +39,15 @@ using namespace std;
 //    return 0;
 //}
 
-void thread_test(int n, int& rx, mutex& rmtx)
-{
-    rmtx.lock();
-    for (int i = 0; i < n; ++i)
-    {
-        ++rx;
-    }
-    rmtx.unlock();
-}
+//void thread_test(int n, int rx, mutex* rmtx)
+//{
+//    rmtx->lock();
+//    for (int i = 0; i < n; ++i)
+//    {
+//        ++rx;
+//    }
+//    rmtx->unlock();
+//}
 
 //void test_ref(int & i)
 //{
@@ -60,17 +60,46 @@ void thread_test(int n, int& rx, mutex& rmtx)
 //    test_ref(i);
 //}
 
+//int main()
+//{
+//    mutex mtx;
+//    int x = 0;
+//
+//    thread t1(thread_test, 200000, x, &mtx);
+//    //thread t2(thread_test, 200000, x,mtx);
+//    //thread t2(thread_test, 300000, ref(x), ref(mtx));
+//
+//    t1.join();
+//    //t2.join();
+//
+//    cout << x << endl;
+//    return 0;
+//}
+
+void thread_test(int n, int& rx, mutex& rmtx)
+{
+    rmtx.lock();
+    for (int i = 0; i < n; ++i)
+    {
+        ++rx;
+    }
+    rmtx.unlock();
+}
+
 int main()
 {
     mutex mtx;
+
     int x = 0;
 
-    thread t1(thread_test, 200000, ref(x), ref(mtx));
-    thread t2(thread_test, 300000, ref(x), ref(mtx));
+    thread t1(thread_test, 100000, ref(x), ref(mtx));
+    thread t2(thread_test, 200000, ref(x), ref(mtx));
+
 
     t1.join();
     t2.join();
 
     cout << x << endl;
+
     return 0;
 }
