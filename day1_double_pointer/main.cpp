60905f9912ecbb834047b956eaf91374b449cd95
有趣的中国人 <dongsijie8@gmail.com>
2025-11-13 23:50:51 +0800

    增加复写 0

--- day1_double_pointer/main.cpp
+++ day1_double_pointer/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 
+
 using namespace std;
 
 class Solution
@@ -24,4 +25,64 @@ public:
             }
         }
     }
+
+    // 复写 0
+    void duplicateZeros(vector<int>& arr)
+    {
+        int cur = 0;
+        int dest = -1;
+        // 1. 先找到最后一个要复写的数字
+        while(cur < arr.size())
+        {
+            if (arr[cur] != 0)
+            {
+                ++dest;
+                cout << dest << endl;
+            }
+            else
+            {
+                dest += 2;
+                cout << dest << endl;
+            }
+            if (dest < arr.size() - 1)
+                ++cur;
+            else
+                break;
+        }
+
+        // 如果最后一个是复写的 0，并且越界了，要做特殊处理
+        if (dest == arr.size())
+        {
+            arr[dest - 1] = 0;
+            dest -= 2;
+            --cur;
+        }
+
+        //往前复写，画图
+        while(dest >= 0)
+        {
+            if (arr[cur] != 0)
+            {
+                arr[dest] = arr[cur];
+                --dest;
+            }
+            else
+            {
+                arr[dest] = arr[dest - 1] = 0;
+                dest -= 2;
+            }
+            --cur;
+        }
+    }
 };
+
+int main()
+{
+    // vector 的size是size_t类型的
+    vector<int> v = {1,0,2,3,0,4,5,0};
+    int i = -1;
+
+    cout << decltype(v.size())(i) << endl;
+    cout << (i < v.size()) << endl;
+    return 0;
+}
