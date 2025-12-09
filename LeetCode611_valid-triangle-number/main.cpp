f025def8dd41d54459f48c8cb7c4d12ad9301f42
有趣的中国人 <dongsijie8@gmail.com>
2025-12-09 09:30:41 +0800

    LeetCode611 有效三角形个数

--- LeetCode611_valid-triangle-number/main.cpp
+++ LeetCode611_valid-triangle-number/main.cpp
@@ -9,12 +9,16 @@ public:
     {
         sort(nums.begin(), nums.end());
         int ret = 0;
+        // 1. 先固定最大的数字
         for (int i = nums.size() - 1; i > 1; --i)
         {
             int left = 0;
             int right = i - 1;
             while (left < right)
             {
+                // 对于次大的数和最小的数相加大于最大的数
+                // 那么对于次大的数字来说，都能和比他小的数字与最大的数字组成三角形
+                // 所以对于次大的数字与最大的数字来说就有right - left 个数字可以组成三角形
                 if (nums[left] + nums[right] > nums[i])
                 {
                     ret += (right - left);
@@ -22,6 +26,8 @@ public:
                 }
                 else
                 {
+                    // 如果次大的数字和最小的数字相加小于最大的数字
+                    // 那么对于最小的数字来说就不能和比他大的任意一个数字与最大的数组成三角形，就++left；
                     ++left;
                 }
             }
