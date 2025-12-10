144841d6685bbe543d2b077be1cd90c6587e3b8a
有趣的中国人 <dongsijie8@gmail.com>
2025-12-10 09:27:11 +0800

    三数之和

---
+++ LeetCode_3sum/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        vector<vector<int>> vv;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; ++i)
+        {
+            int left = i + 1, right = nums.size() - 1;
+
+            while (left < right)
+            {
+                if (nums[left] + nums[right] + nums[i] > 0)
+                {
+                    --right;
+                }
+                else if (nums[left] + nums[right] + nums[i] < 0)
+                {
+                    ++left;
+                }
+                else
+                {
+                    vv.push_back({nums[left], nums[right], nums[i]});
+                    while (left < right && nums[left] == nums[left + 1])
+                    {
+                        ++left;
+                    }
+
+                    while (left < right &&nums[right] == nums[right - 1])
+                    {
+                        --right;
+                    }
+
+                    if (left < right)
+                    {
+                        ++left;
+                        --right;
+                    }
+                    else
+                        break;
+                }
+
+
+            }
+
+            while (nums[i] == nums[i + 1] && i < nums.size() - 2)
+            {
+                ++i;
+            }
+        }
+        return vv;
+    }
+};
